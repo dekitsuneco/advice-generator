3142595804b1968c6cc451593ba78980b7f0dae6
dekitsuneco <47333988+dekitsuneco@users.noreply.github.com>
2022-04-29 06:04:18 +0500

    Create UI-KIT dir and global styles to it

---
+++ src/ui-kit/styles/variables.ts
@@ -0,0 +1,19 @@
+export const variables = {
+  color: {
+    primary: 'hsl(193, 38%, 86%)',
+    primaryDark: 'hsl(150, 100%, 66%)',
+    neutral: 'hsl(217, 19%, 38%)',
+    neutralDark: 'hsl(217, 19%, 24%)',
+    neutralDarker: 'hsl(218, 23%, 16%)',
+  },
+  fontSize: {
+    medium: '28px',
+  },
+  fontFamily: {
+    regular: 'Manrope',
+  },
+  breakpoints: {
+    desktop: '1440px',
+    mobile: '375px',
+  },
+};
